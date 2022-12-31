f3623dba4aa4a7c1200a9c4e4139b897aeb8dc80
Cristina Tiznado <cristina.tiznado7@gmail.com>
2022-12-31 17:55:40 -0300

    cambios vecino

--- frontend/pages/Vecino/agregar_reserva.jsx
+++ frontend/pages/Vecino/agregar_reserva.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useRef, useState } from 'react'
-import { Flex, Text, Box, Stack,Button,VStack,HStack, Input, Select, Menu, MenuButton, MenuList,MenuItem } from "@chakra-ui/react";
+import { useDisclosure,DrawerOverlay,DrawerContent,DrawerHeader,DrawerBody,DrawerFooter,Drawer,Flex,InputGroup, InputLeftElement, Text, Box, Stack,Button,VStack,HStack, Input, Select} from "@chakra-ui/react";
 import Swal from 'sweetalert2'
 import axios from 'axios'
 import { useRouter } from "next/router";
 
-const AgregarReserva = () =>{
-    const router0 = useRouter();
+const AgregarReserva = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const router = useRouter();
     const {
         query: { codigo },
-    } = router0;
+    } = router;
 
     const props = {
         codigo,
@@ -40,18 +41,17 @@ const AgregarReserva = () =>{
 
     const [selectedOption, setSelectedOption] = useState('')
     const [valor, setValor] = useState('0')
-    const [valor2, setValor2] = useState('0')
     const [open, setOpen] = useState(false)
+    const [calendar, setCalendar] = useState('')
     const refOne = useRef(null)
-    const router = useRouter();
 
-    const [values, setValues] = useState({
+    const [values, setValues]= useState({
         dia:'',
         mes:'',
         year:'',
         hora:'',
         servicio:'',
-        vecino:'',
+        vecino: getVecino(),
         costo_base:'',
         costo_extra: 0
     })
@@ -76,7 +76,8 @@ const AgregarReserva = () =>{
 
     const onChange = async (e) => {
         if(e.target.name=="servicio"){
-            const response1 = await axios.get(`${process.env.API_URL}/servicio/search/${e.target.value}`)
+
+            const response1 = await axios.get(`${process.env.API_URL}/servicio/search/${codigo}`)
             setValor(response1.data.costo)
             setValues({
                 ...values,
@@ -85,7 +86,7 @@ const AgregarReserva = () =>{
                 })
                 console.log(e.target.name, response1.data.costo);
         }else
-        if(e.target.name != "servicio"){
+        if( e.target.name != "servicio"){
             setValues({
                 ...values,
                 [e.target.name]:e.target.value
@@ -99,9 +100,15 @@ const AgregarReserva = () =>{
     const DateSetter = (e) =>
     {
         const string = e.target.value
-        const year = string.substring(0,4)
-        const mes = string.substring(5,7)
-        const dia = string.substring(8,10)
+        const timestamp = Date.parse(string)
+        const date2 = new Date(timestamp)
+
+        const a=date2.getDate()+1;
+        const b=date2.getMonth()+1;
+        const c=date2.getFullYear();
+        const dia=  a.toString();
+        const mes=  b.toString();
+        const year= c.toString();
 
         setValues({...values,dia,mes,year});
     }
@@ -122,7 +129,9 @@ const AgregarReserva = () =>{
         return fechaMinima;
     }
 
-    function castMax(){
+
+    function castMax()
+    {
         const currentDate = new Date();
         const dateString1 = currentDate.toLocaleDateString('es-ES', { day: '2-digit' });
         const dateString2 = currentDate.toLocaleDateString('es-ES', { month: '2-digit' });
@@ -143,18 +152,16 @@ const AgregarReserva = () =>{
         }
     }
 
+
+
     const onSubmit= async (e) =>{
+
         e.preventDefault()
-        getVecino()
         console.log(values)
 
         try {
-            setValues({
-                ...values,
-                vecino:vecino1
-                })
-                console.log("vecino:",values.vecino);
-        const response = await axios.post(`${process.env.API_URL}/reserva/${props.codigo}`,values)
+        const response = await axios.post(`${process.env.API_URL}/reserva/${codigo}`,values)
+        console.log(response)
 
         if(response.status===201){
             Swal.fire({
@@ -176,6 +183,8 @@ const AgregarReserva = () =>{
         }
     }
 
+    
+
     const [servicios, setServicios] = useState([])
     const getServicios = async () => {
     const response = await axios.get(`${process.env.API_URL}/servicios`)
@@ -183,112 +192,102 @@ const AgregarReserva = () =>{
     }
 
     const showServicios= () =>{
+
         return servicios.map(servicios =>{
         return (
+
             <option name="servicio" key={servicios.nombre} value={servicios._id}>{servicios.nombre}</option>
         )
         })
     }
-
-    const Tot = () => {
-        if(!valor2)
-        {
-            const result = valor
-            return result
-        }
-        const result = parseInt(valor, 10) + parseInt(valor2, 10)
-        return result
-    }
-
 
 return (
     <Flex
             flexDirection="column"
             width="100wh"
             height="100vh"
-            backgroundColor="blue.300"
+            backgroundColor="blue.400"
             alignItems="center"
             >
-                <Box backgroundColor="blue.500" w={"100%"} h="10">
-    <Menu>
-  <MenuButton  color="white" w="10%" h="10" background={"blue.600"}>
-    Menú
-  </MenuButton>
-  <MenuList >
-    <MenuItem color="blue.400" as="b"  onClick={() => router.push("/Admin/inicio_admin")} >Inicio</MenuItem>
-    <MenuItem color="blue.400" as="b"  onClick={() => router.push("/Admin/Reservas/reservas_admin")} >Reservas</MenuItem>
-    <MenuItem color="blue.400" as="b" onClick={() => router.push("/Admin/Gastos/gastos_admin")}>Gastos</MenuItem>
-    <MenuItem color="blue.400" as="b" onClick={() => router.push("/Admin/Mensajes/mensajes_admin")}>Mensajes</MenuItem>
-    <MenuItem color="blue.400" as="b" onClick={() => router.push("/Admin/Multas/multas_admin")}>Multas</MenuItem>
-    <MenuItem color="blue.400" as="b" onClick={() => router.push("/Admin/Mantenciones/mantenciones_admin")}>Manteciones</MenuItem>
-    <MenuItem color="blue.400" as="b" onClick={() => router.push("/Admin/Vecino/vecinos_admin")}>Vecinos</MenuItem>
-  </MenuList>
-</Menu>
-    </Box>
-    <Button mt={10} name="atras" colorScheme="blue" as="b" rounded="40" style={{
-    position: "fixed",
-    top: "20px",
-    left: "200px",
-    zIndex: 1,
-    }}
-    onClick={()=>router.push("/Admin/Reservas/reservas_admin")}>
-    Volver atrás</Button>
+                <Box backgroundColor="blue.500" w={"100%"} h="16">
+        <Button colorScheme='blue' onClick={onOpen} h="16">
+        Menu
+       </Button>
+       <Button colorScheme='blue' marginLeft="80%" onClick={()=>router.push("/")} h="16">
+        Cerrar Sesión
+       </Button>
+       </Box>
+
+        <Button mt={10} name="atras" colorScheme="blue" as="b" rounded="40" marginLeft="-60%"
+        onClick={()=>router.push("/Vecino/reservas_vecino")}>
+        Volver atrás</Button>
+
+        <Drawer placement='left'  onClose={onClose} isOpen={isOpen} >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader  backgroundColor="blue.500" color="white">Menu</DrawerHeader>
+          <DrawerBody backgroundColor="blue.300">
+            <Button width={"100%"} colorScheme="blue" mb="2" height="20" fontSize="20">Inicio</Button>
+            <Button width={"100%"} colorScheme="blue" mb="2" height="20" fontSize="20">Reservas</Button>
+            <Button width={"100%"} colorScheme="blue" mb="2" height="20" fontSize="20">Gastos</Button>
+            <Button width={"100%"} colorScheme="blue" mb="2" height="20" fontSize="20">Mensajes</Button>
+            <Button width={"100%"} colorScheme="blue" mb="2" height="20" fontSize="20">Multas</Button>
+
+          </DrawerBody>
+          <DrawerFooter backgroundColor="blue.300">
+            <Button mr = {3} onClick={onClose} colorScheme="blue">
+              Cerrar
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
               <Text fontSize={50} color="white" mt={30} mb={30}>Crear Reserva</Text>
               <Box  minW={{ base: "10%", md: "468px"}} >
-            <form>
-                <Stack spacing={4}
-                    p="1rem"
-                    backgroundColor="whiteAlpha.900"
-                    boxShadow="md"
-                    rounded="16"
-                    flexDir="column"
-            mb="2"
-            justifyContent="left"
-            alignItems="left">
-                <HStack>
-                    <VStack spacing={6}>
-                            <HStack>
-
-                            </HStack>
-                            <HStack>
-                                    <Text color={"blue.400"} as="b" >Fecha:</Text>
-                                    <Input type="date" id="start"
-                                        date={new Date()}
-                                        onChange={DateSetter}
-                                        min={castMin()} max={castMax()}></Input>
-                            </HStack>
-
-                            <HStack>
-                                    <Text color={"blue.400"} as="b" >Hora:</Text>
-                                    <Input width={60}
-                                    type="time"
-                                    pattern="[0-9]{2}:[0-9]{2}" name={"hora"} onChange={onChange} step={3600}></Input>
-                            </HStack>
-                            <HStack>
-                                    <Text  value={selectedOption} color={"blue.400"} name="servicio" as="b" >Servicio:</Text>
-                                    <Select placeholder='seleccione servicio' name="servicio" onChange={onChange}>
-                                        {showServicios()}
-                                    </Select>
-                            </HStack>
-                            <HStack>
-                                    <Text color={"blue.400"} as="b" >Costo del Servicio</Text>(
-                                    <Text name='costo_base'>{"$"+valor}</Text>)
-
-                            </HStack>
-                    </VStack>
-
-                    </HStack>
-                                <Button mb="2"
-                                    variant="solid"
-                                    colorScheme="blue"
-                                    rounded="50"
-                                    onClick={onSubmit}
-                                    >
-                                        CREAR
-                                </Button>
-                </Stack>
-            </form>
-        </Box>
+                    <Stack
+                        spacing={4}
+                        p="1rem"
+                        backgroundColor="whiteAlpha.900"
+                        boxShadow="md"
+                        rounded="16"
+                        flexDir="column"
+                        mb="2"
+                        justifyContent="left"
+                        alignItems="left">
+                                <HStack mt={6}>
+                                        <Text color={"blue.400"} as="b">Fecha:</Text>
+                                        <Input type="date" id="start"
+                                            date={new Date()}
+                                            onChange={DateSetter}
+                                            min={castMin()} max={castMax()}></Input>
+                                </HStack>
+    
+                                <HStack>
+                                        <Text color={"blue.400"} as="b" >Hora:</Text>
+                                        <Input width={"full"}
+                                        type="time"
+                                        pattern="[0-9]{2}:[0-9]{2}" name={"hora"} onChange={onChange} step={3600}></Input>
+                                </HStack>
+                                <HStack>
+                                        <Text  value={selectedOption} color={"blue.400"} name="servicio" as="b" >Servicio:</Text>
+                                        <Select placeholder='Seleccione servicio' name="servicio" onChange={onChange}>
+                                            {showServicios()}
+                                        </Select>
+                                </HStack>
+                                <HStack>
+                                        <Text color={"blue.400"} as="b" >Costo del Servicio: </Text>(
+                                        <Text name='costo_base'>{"$"+valor}</Text>)
+    
+                                </HStack>
+                                    <Button mb={2}
+                                        variant="solid"
+                                        colorScheme="blue"
+                                        rounded="50"
+                                        onClick={onSubmit}
+                                        >
+                                            CREAR
+                                    </Button>
+                    </Stack>
+            </Box>
 
             </Flex>
 )
